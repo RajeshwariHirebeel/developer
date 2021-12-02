6a4cded1196b520467b6df7d2c4cae87aff33eaf
unknown <rajeshwaripundaleek@gmail.com>
2021-12-02 21:10:22 +0530

    welcome to gitlab

---
+++ src/test/java/APITests.java
@@ -0,0 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.xml.xpath.XPath;
+
+public class APITests {
+    @Test
+    void test1()
+    {
+
+        Response response = RestAssured.get( "https://reqres.in/api/users?page=2");
+        System.out.println("Response: "+response.asString());
+        System.out.println("Status Code : "+response.getStatusCode());
+        System.out.println("Body: "+response.getBody().asString());
+        System.out.println("Time taken : "+response.getTime());
+        System.out.println("Header : "+response.getHeader( "content-type"));
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals();
+
+    }
+
+}
